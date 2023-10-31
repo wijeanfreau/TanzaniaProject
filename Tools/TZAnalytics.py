9c8092db0c346ad63ec9bbdef51ff33322d4caae
wijeanfreau <winthrop.isaac@gmail.com>
2023-10-30 18:39:54 -0600

    Created Graphs of Teacher Training Exams

--- Tools/TZAnalytics.py
+++ Tools/TZAnalytics.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-basepath = 'V:\FHSS-JoePriceResearch\RA_work_folders\Winthrop_Jeanfreau\TanzaniaProject\FinalCSVs\TeacherTrainingExam20-23.csv'
+basepath = r'C:\Users\Winthrop Isaac\OneDrive\Documents\GitHub\TanzaniaProject\FinalCSVs\TeacherTrainingExam20-23.csv'
 
 df = pd.read_csv(basepath, index_col=False)
+print(df.head(10))
+
+##GRAPHING GPA
 #Remove non-numeric entries
-df['AGGT'] = pd.to_numeric(df['AGGT'], errors='coerce')
-df['AGGT'] = df['AGGT'].notnull()
+df = df[pd.to_numeric(df['AGGT'], errors ='coerce').notnull()]
+#Make the remaining entries numeric
+df['AGGT'] = pd.to_numeric(df['AGGT'])
 summary = df['AGGT'].mean()
 print(f'The mean score among students is: ', summary)
 graph = df['AGGT'].plot(kind='kde')
-print(graph)S
+plt.show()
+
+##GRAPH GPA DENSITY BY GENDER
+plt.clf()
+data = df[['AGGT','SEX']]
+sns.set_style('whitegrid')
+sns.kdeplot(data=data, x = 'AGGT', hue = 'SEX', fill = True, common_norm=False)
+
+plt.xlabel('GPA')
+plt.ylabel('Density')
+plt.title('Density Plot of GPA by Gender')
+
+plt.show()
